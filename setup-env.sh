68bbd7471cad6767141a6a6d71597ebe10e13e70
Efren Ospino <eospino@wawand.co>
2023-08-24 10:55:04 -0500

    fix: use source to run bash

--- setup-env.sh
+++ setup-env.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
 echo "$SERVICE_CREDENTIALS_JSON" | base64 -d > firebase.json
 echo "$KEYSTORE_CONTENT" | base64 -d > keystore.jks
 
-SERVICE_CREDENTIALS_PATH="$(readlink -f firebase.jks)"
-export SERVICE_CREDENTIALS_PATH
-
-KEYSTORE_PATH="$(readlink -f keystore.jks)"
-export KEYSTORE_PATH
+export SERVICE_CREDENTIALS_PATH="$(pwd)"/"$(basename firebase.json)"
+export KEYSTORE_PATH="$(pwd)"/"$(basename keystore.jks)"
