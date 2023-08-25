a4ed1da577ae89d34778be9825605db4b270c20d
Efren Ospino <eospino@wawand.co>
2023-08-25 14:29:28 -0500

    fix: set env var to GITHUB_ENV

--- setup-env.sh
+++ setup-env.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
 echo "$SERVICE_CREDENTIALS_JSON" | base64 -d > firebase.json
 echo "$KEYSTORE_CONTENT" | base64 -d > keystore.jks
+
+SERVICE_CREDENTIALS_PATH="$(pwd)"/"$(basename firebase.json)"
+KEYSTORE_PATH="$(pwd)"/"$(basename keystore.jks)"
+
+echo "SERVICE_CREDENTIALS_PATH=$SERVICE_CREDENTIALS_PATH" >> "$GITHUB_ENV"
+echo "KEYSTORE_PATH=$KEYSTORE_PATH" >> "$GITHUB_ENV"
