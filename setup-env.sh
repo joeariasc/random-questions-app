d4fe22b214cfaa8091f76fbfc182b72a912fd90b
Efren Ospino <eospino@wawand.co>
2023-08-25 14:21:48 -0500

    fix: set env var correctly

--- setup-env.sh
+++ setup-env.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
 echo "$SERVICE_CREDENTIALS_JSON" | base64 -d > firebase.json
 echo "$KEYSTORE_CONTENT" | base64 -d > keystore.jks
-
-export SERVICE_CREDENTIALS_PATH="$(pwd)"/"$(basename firebase.json)"
-export KEYSTORE_PATH="$(pwd)"/"$(basename keystore.jks)"
