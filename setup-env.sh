4dcab3b60c3f5ef55aeb027e4842c26d102de62c
Efren Ospino <efrenospinojr@gmail.com>
2023-08-25 15:10:11 -0500

    fix: update gh actions yml files to fix jobs

--- setup-env.sh
+++ setup-env.sh
@@ -2,9 +2,3 @@
 
 echo "$SERVICE_CREDENTIALS_JSON" | base64 -d > firebase.json
 echo "$KEYSTORE_CONTENT" | base64 -d > keystore.jks
-
-SERVICE_CREDENTIALS_PATH="$(pwd)"/"$(basename firebase.json)"
-KEYSTORE_PATH="$(pwd)"/"$(basename keystore.jks)"
-
-echo "SERVICE_CREDENTIALS_PATH=$SERVICE_CREDENTIALS_PATH" >> "$GITHUB_ENV"
-echo "KEYSTORE_PATH=$KEYSTORE_PATH" >> "$GITHUB_ENV"
