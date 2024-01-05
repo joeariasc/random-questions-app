5e22b578bab4cc680cf40529458fdc67bd42065e
Joe Arias <jarias@wawand.co>
2024-01-05 09:59:02 -0500

    removing unused files

--- app/src/main/kotlin/com/spotapp/mobile/app/di/DomainModule.kt
+++ app/src/main/kotlin/com/spotapp/mobile/app/di/DomainModule.kt
@@ -1,10 +1,7 @@
 package com.spotapp.mobile.app.di
 
 import com.spotapp.mobile.domain.usecases.AuthenticateNewUserWithEmail
-import com.spotapp.mobile.domain.usecases.SignUpUserFirebase
 import com.spotapp.mobile.domain.usecases.GetCurrentAppState
-import com.spotapp.mobile.domain.usecases.GetUserInformation
-import com.spotapp.mobile.domain.usecases.SignInUserFirebase
 
 class DomainModule(private val dataModule: DataModule) {
 
@@ -13,13 +10,4 @@ class DomainModule(private val dataModule: DataModule) {
 
     val getCurrentAppStateUseCase: GetCurrentAppState
         get() = GetCurrentAppState(dataModule.usersRepository)
-
-    val signUpUserFirebase: SignUpUserFirebase
-        get() = SignUpUserFirebase(dataModule.usersRepository)
-
-    val signInUserFirebase: SignInUserFirebase
-        get() = SignInUserFirebase(dataModule.usersRepository)
-
-    val getUserInformation: GetUserInformation
-        get() = GetUserInformation(dataModule.usersRepository)
 }
