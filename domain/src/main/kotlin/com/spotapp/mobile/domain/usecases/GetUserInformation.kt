aca944f7d1f8d01cd43c9e9c85a62620f9450f75
Joe Arias <jarias@wawand.co>
2024-01-05 09:58:01 -0500

    removing unused files

--- domain/src/main/kotlin/com/spotapp/mobile/domain/usecases/GetUserInformation.kt
+++
@@ -1,8 +0,0 @@
-package com.spotapp.mobile.domain.usecases
-
-import com.spotapp.mobile.data.repository.UsersRepository
-
-class GetUserInformation(private val usersRepository: UsersRepository) {
-    suspend operator fun invoke(): Pair<String?, String?> =
-        usersRepository.getUserInformation()
-}
