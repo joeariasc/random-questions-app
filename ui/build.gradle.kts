568e3ab9a8cd2ea7d171172295b0d701263cadaf
Joe Arias <jarias@wawand.co>
2024-01-05 09:58:42 -0500

    loading data module for ui module

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 dependencies {
 
     implementation(project(":domain"))
+    implementation(project(":data"))
 
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.core.ktx)
