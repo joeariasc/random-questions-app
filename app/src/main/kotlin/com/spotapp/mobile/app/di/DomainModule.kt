fa20bd4ba1ae76136599d1ebb114824690876b88
Joe Arias <jarias@wawand.co>
2024-01-16 16:34:13 -0500

    adding use cases to register and update users

--- app/src/main/kotlin/com/spotapp/mobile/app/di/DomainModule.kt
+++ app/src/main/kotlin/com/spotapp/mobile/app/di/DomainModule.kt
@@ -1,9 +1,17 @@
 package com.spotapp.mobile.app.di
 
 import com.spotapp.mobile.domain.usecases.GetCurrentAppState
+import com.spotapp.mobile.domain.usecases.RegisterUser
+import com.spotapp.mobile.domain.usecases.UpdateUserName
 
 class DomainModule(private val dataModule: DataModule) {
 
     val getCurrentAppStateUseCase: GetCurrentAppState
         get() = GetCurrentAppState(dataModule.usersRepository)
+
+    val registerUser: RegisterUser
+        get() = RegisterUser(usersRepository = dataModule.usersRepository)
+
+    val updateUserName: UpdateUserName
+        get() = UpdateUserName(usersRepository = dataModule.usersRepository)
 }
