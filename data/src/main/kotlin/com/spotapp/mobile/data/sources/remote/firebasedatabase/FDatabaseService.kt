6266fac4de8b53efe8d8a38900d7f594d6e836c0
Joe Arias <jarias@wawand.co>
2024-01-24 17:41:43 -0500

    adjusting daatabase service

--- data/src/main/kotlin/com/spotapp/mobile/data/sources/remote/firebasedatabase/FDatabaseService.kt
+++ data/src/main/kotlin/com/spotapp/mobile/data/sources/remote/firebasedatabase/FDatabaseService.kt
@@ -2,6 +2,7 @@ package com.spotapp.mobile.data.sources.remote.firebasedatabase
 
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -9,6 +10,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.spotapp.mobile.data.sources.remote.firebasedatabase.model.UserData
+import kotlinx.coroutines.tasks.await
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -18,50 +20,75 @@ class FDatabaseService(private val auth: FirebaseAuth) {
     private val firebaseDB: DatabaseReference = Firebase.database.reference
 
     fun subscribeToRealtimeUpdates() {
-        //TODO
+        auth.currentUser?.let {
+            firebaseDB.child("users").addChildEventListener(object: ChildEventListener{
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onChildRemoved(snapshot: DataSnapshot) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
     }
 
-    fun setUserScore(scored: Boolean = true) {
+    suspend fun getRanking() {
+        runCatching {
+            auth.currentUser?.let {
+                firebaseDB.child("users").get().await()
+            }
+        }
+    }
+
+    suspend fun setUserScore(scored: Boolean) {
         runCatching {
             auth.currentUser?.run {
-                firebaseDB.child("users").child(uid)
-                    .addListenerForSingleValueEvent(object : ValueEventListener {
-                        override fun onDataChange(snapshot: DataSnapshot) {
-                            if (snapshot.exists()) {
-                                val userData = snapshot.getValue(UserData::class.java)
-                                firebaseDB.child("users").child(uid).let {
-                                    it.setValue(
-                                        UserData(
-                                            userId = userData?.userId,
-                                            score = if (scored) (userData?.score?.plus(1)) else 0,
-                                            lastConnection = getCurrentTimeFormatted()
-                                        )
-                                    )
-                                }
-                            } else {
-                                firebaseDB.child("users").child(uid).let {
-                                    it.setValue(
-                                        UserData(
-                                            userId = uid,
-                                            score = if (scored) 1 else 0,
-                                            lastConnection = getCurrentTimeFormatted()
-                                        )
-                                    )
-                                }
-                            }
-                        }
-
-                        override fun onCancelled(error: DatabaseError) {
-                            Log.d("FDatabaseService", "onCancelled")
-                        }
-                    })
+                firebaseDB.child("users").child(uid).get().await()
             }
-        }.onSuccess {
+        }.onSuccess { snapshot ->
+            if (snapshot?.exists() == true) {
+                val userData = snapshot.getValue(UserData::class.java)
+                findAndSetScore(
+                    score = if (scored) (userData?.score!!.plus(1)) else 0,
+                    email = userData?.email!!
+                )
+            } else {
+                findAndSetScore(
+                    score = if (scored) 1 else 0,
+                    email = auth.currentUser?.email!!
+                )
+            }
             Log.d("FDatabaseService", "onSuccess")
         }.onFailure {
             Log.d("FDatabaseService", "onFailure, error => ${it.message}")
         }
 
+    }
+
+    private fun findAndSetScore(score: Int, email: String) {
+        firebaseDB.child("users").child(auth.currentUser?.uid!!).let {
+            it.setValue(
+                UserData(
+                    email = email,
+                    score = score,
+                    lastConnection = getCurrentTimeFormatted()
+                )
+            )
+        }
     }
 
     private fun getCurrentTimeFormatted(): String {
