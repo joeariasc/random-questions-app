793ee509633ee942c1201961aeeb440a82aa5c3d
Joe Arias <jarias@wawand.co>
2024-01-24 17:42:10 -0500

    updating user data model

--- data/src/main/kotlin/com/spotapp/mobile/data/sources/remote/firebasedatabase/model/UserData.kt
+++ data/src/main/kotlin/com/spotapp/mobile/data/sources/remote/firebasedatabase/model/UserData.kt
@@ -1,11 +1,7 @@
 package com.spotapp.mobile.data.sources.remote.firebasedatabase.model
 
 data class UserData(
-    val userId: String?,
-    val score: Int?,
-    val lastConnection: String?,
-) {
-    //Note: this is needed to read the data from the firebase database
-    //firebase database throws this exception: UserData does not define a no-argument constructor
-    constructor() : this(null, null, null)
-}
+    val email: String? = null,
+    val score: Int? = null,
+    val lastConnection: String? = null,
+)
